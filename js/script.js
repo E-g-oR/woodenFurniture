cad808d1522a91c650f588094c6b759fc4a615ce
Egor <68065928+E-g-oR@users.noreply.github.com>
2020-10-31 14:06:47 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 var MenuButton = document.querySelector(".menu-button"),
-  Menu = document.querySelector(".menu");
+  Menu = document.querySelector(".menu"),
+  Nav = document.querySelector('.nav');
 
 MenuButton.addEventListener("click", () => {
   MenuButton.classList.toggle("menu_active");
   Menu.classList.toggle("menu_active");
+  Nav.classList.toggle("menu_active");
 });
 document.addEventListener("scroll", () => {
   if (MenuButton.classList.contains("menu_active")) {
     MenuButton.classList.remove("menu_active");
     Menu.classList.remove("menu_active");
+    Nav.classList.remove("menu_active");
+
   }
 });
